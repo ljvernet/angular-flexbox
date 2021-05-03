ddb0493947dda09b0dccb9dc934fbe8d3288ecc9
ljvernet <ljvernet@hotmail.com>
2021-05-03 08:40:27 +0200

    nivell 2 exercici 2
    añadimos artículos y efecto leer más

---
+++ scripts/desplegar.js
@@ -0,0 +1,23 @@
+
+        function vermas(id){
+        if(id=="mas"){
+        document.getElementById("desplegar").style.display="block";   
+        document.getElementById("mas").style.display="none"; 
+        }
+        else{
+        document.getElementById("desplegar").style.display="none";
+        document.getElementById("mas").style.display="inline";
+        }
+        }
+        
+         function vermas2(id){
+            if(id=="mas2"){
+            document.getElementById("desplegar2").style.display="block";   
+            document.getElementById("mas2").style.display="none"; 
+            }
+            else{
+            document.getElementById("desplegar2").style.display="none";
+            document.getElementById("mas2").style.display="inline";
+            }
+            }
+    
